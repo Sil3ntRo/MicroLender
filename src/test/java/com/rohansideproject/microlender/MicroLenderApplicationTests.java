81c2af8fd236ec84ad96167f4af06fb4c54e185e
Sil3ntRo <rgayle33614@gmail.com>
2022-07-11 16:55:55 -0400

    first commit

---
+++ src/test/java/com/rohansideproject/microlender/MicroLenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohansideproject.microlender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroLenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
