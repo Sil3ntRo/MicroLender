59d9d13ec7e5c5350c3836f629ae0b419922d13b
Sil3ntRo <rgayle33614@gmail.com>
2022-07-12 21:35:39 -0400

    Created Loan Entity

---
+++ src/main/java/com/rohansideproject/microlender/domain/model/LoanRequest.java
@@ -0,0 +1,62 @@
+package com.rohansideproject.microlender.domain.model;
+
+import java.time.Duration;
+import java.util.Objects;
+
+public class LoanRequest {
+	
+	private final int amount;
+	private final User borrower;
+	private final Duration repaymentTerm;
+	private final double interestRate;
+	
+	public LoanRequest(int amount, User borrower, Duration repaymentTerm, double interestRate) {
+		this.amount = amount;
+		this.borrower = borrower;
+		this.repaymentTerm = repaymentTerm;
+		this.interestRate = interestRate;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public User getBorrower() {
+		return borrower;
+	}
+
+	public Duration getRepaymentTerm() {
+		return repaymentTerm;
+	}
+
+	public double getInterestRate() {
+		return interestRate;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(amount, borrower, interestRate, repaymentTerm);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		LoanRequest other = (LoanRequest) obj;
+		return amount == other.amount && Objects.equals(borrower, other.borrower)
+				&& Double.doubleToLongBits(interestRate) == Double.doubleToLongBits(other.interestRate)
+				&& Objects.equals(repaymentTerm, other.repaymentTerm);
+	}
+
+	@Override
+	public String toString() {
+		return "LoanRequest [amount=" + amount + ", borrower=" + borrower + ", repaymentTerm=" + repaymentTerm
+				+ ", interestRate=" + interestRate + "]";
+	}
+	
+	
+}
---
+++ src/main/java/com/rohansideproject/microlender/domain/model/User.java
@@ -0,0 +1,63 @@
+package com.rohansideproject.microlender.domain.model;
+
+import java.util.Objects;
+
+public final class User {
+	
+	private final String firstName;
+	private final String lastName;
+	private final int age;
+	private final String occupation;
+	
+	
+	public User(String firstName, String lastName, int age, String occupation) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+		this.occupation = occupation;
+	}
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+	public String getOccupation() {
+		return occupation;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(age, firstName, lastName, occupation);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User user = (User) obj;
+		return age == user.age && Objects.equals(firstName, user.firstName)
+				&& Objects.equals(lastName, user.lastName) && Objects.equals(occupation, user.occupation);
+	}
+
+
+	
+	
+}
